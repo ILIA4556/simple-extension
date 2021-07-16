919440a0adb1edd50b66b8e1688e725f4f5eea52
ILIA4556 <87526128+ILIA4556@users.noreply.github.com>
2021-07-16 19:38:20 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.coral0, function (sprite, location) {
+    game.over(true)
+})
+let myCorg = corgio.create(SpriteKind.Player)
+myCorg.verticalMovement()
+myCorg.horizontalMovement()
+myCorg.updateSprite()
+myCorg.horizontalMovement()
+myCorg.follow()
+tiles.setTilemap(tilemap`level1`)
